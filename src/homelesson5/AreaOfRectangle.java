24637df32070b100c0752653fee55dd3b51a42e1
Ion Sandu <sanduion787@gmail.com>
2022-02-04 12:11:52 +0200

    "Adaug modificarie"

--- src/homelesson5/AreaOfRectangle.java
+++ src/homelesson5/AreaOfRectangle.java
@@ -11,14 +11,22 @@ public class AreaOfRectangle {
         System.out.println("Enter the width of Rectangle:");
 
         double width1 = sc.nextDouble();
-        double area1 = length1*width1;
-        System.out.println("Area of Rectangle is:"+area1);
+        double area1 = length1 * width1;
+        System.out.println("Area of Rectangle is:" + area1);
 
         double length2 = sc.nextDouble();
         System.out.println("Enter the width of Rectangle:");
 
         double width2 = sc.nextDouble();
-        double area2 = length2*width2;
-        System.out.println("Area of Rectangle is:"+area2);
+        double area2 = length2 * width2;
+        System.out.println("Area of Rectangle is:" + area2);
+
+        if (length1 * width1 > length2 * width2) {
+            System.out.println("Primul dreptunghi are aria mai mare decit la al doilea!");
+
+        } else if (length2 * width2 >length1 * width1) {
+            System.out.println("Al doilea dreptunghi are aria mai mare decit la primul!");
+
+        }
     }
 }
