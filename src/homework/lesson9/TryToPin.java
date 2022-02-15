ef7c1603e3a7424819bee35d7458b90a3977921b
Ion Sandu <sanduion787@gmail.com>
2022-02-15 16:36:50 +0200

    "Modificari1."

--- src/homework/lesson9/TryToPin.java
+++ src/homework/lesson9/TryToPin.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 
 public class TryToPin {
     public static void main(String[] args) {
-        final String PASSWORD = "1234";
+        final String Pin = "1234";
 
         for (int passAttempts = 0; passAttempts < 3; passAttempts++) {
             System.out.print("\nEnter Your Password: ");
             Scanner input = new Scanner(System.in);
             String inputPass = input.nextLine();
 
-            if (inputPass.equals(PASSWORD)) {
+            if (inputPass.equals(Pin)) {
                 System.out.println("\nCorrect, welcome back!");
                 break;
             } else {
-                if (!(inputPass.equals(PASSWORD))) {
+                if (!(inputPass.equals(Pin))) {
                     System.out.println("\nWrong Password Try Again");
                 }
                 }
