3a3a7503f66929d86d7ced64156a6cc3df39e83f
Ion Sandu <sanduion787@gmail.com>
2022-02-16 17:00:15 +0200

    "lesson10"

---
+++ src/homework/lesson10/Months.java
@@ -0,0 +1,20 @@
+package homework.lesson10;
+
+public enum Months {
+    JAN, FEB, MAR, APR, MAY, JUN,
+    JUL, AUG, SEP, OCT, NOV, DEC;
+
+    public static void main(String[] args) {
+
+        System.out.println("Months of year are:");
+
+        for (Months month_name : Months.values())
+
+            System.out.println(month_name);
+    }
+}
+
+
+
+
+
